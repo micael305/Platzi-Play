2d9bd2364abea709f54e441043fa5351b8a466d3
micael305 <miicael305@gmail.com>
2026-01-10 11:16:03 -0300

    Creacion de un endpoint REST con Spring Boot e integracion con IA usando LangChain4j

---
+++ src/main/java/com/platzi/play/HelloController.java
@@ -0,0 +1,17 @@
+package com.platzi.play;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final PlatziPlayAiService aiService;
+    public HelloController(PlatziPlayAiService aiService){
+        this.aiService = aiService;
+    }
+    @GetMapping("/")
+    public String hello(){
+        return this.aiService.generateGreeting();
+    }
+}
